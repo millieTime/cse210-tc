a0a382378fef7e6d5404acf3d0a0e21360344a89
millieTime <thisismynewemailsoyeah@gmail.com>
2021-01-28 16:26:23 -0700

    creating director base branch

---
+++ HiLo_Project/hilo/game/director.py
@@ -0,0 +1,31 @@
+# This is the director class
+
+from game.thrower import Thrower
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+    
+    Attributes:
+
+    """
+
+    def __init__(self):
+        """The class constructor.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.keep_playing = True
+        self.score = 0
+        self.drawer = Drawer()
+
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            # do logic
