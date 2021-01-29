bc65a1e0f692dcb11f6d6d1658010242f73c368f
millieTime <thisismynewemailsoyeah@gmail.com>
2021-01-28 20:27:49 -0700

    Main loop and main processing methods complete.

--- HiLo_Project/hilo/game/director.py
+++ HiLo_Project/hilo/game/director.py
@@ -16,17 +16,43 @@ class Director:
             self (Director): an instance of Director.
         """
         self.keep_playing = True
-        self.score = 0
+        self.score = 300
         self.drawer = Drawer()
 
     def start_game(self):
         """Starts the game loop to control the sequence of play.
+        
         Args:
             self (Director): an instance of Director.
         """
         while self.keep_playing:
-            # do logic
-            pass
+            self.acquire_guess()
+            self.evaluate_guess()
+            keep_playing = self.score > 0 and self.user_play_again()
+
+    def acquire_guess(self):
+        """Causes a new card to be drawn, displays it,
+        and gets the user's guess.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.drawer.draw_new_card()
+        print("The card is:", self.getCurrentCard())
+        self.guess = self.getUserInput()
+    
+    def evaluate_guess(self):
+        """Causes a new card to be drawn and displays it.
+        Updates the score and displays it.
+
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.drawer.draw_new_card()
+        print("Next card was:", self.getCurrentCard())
+        comparison = self.compare_inputs(self.guess)
+        self.update_points(comparison)
+        print("Your score is:", self.score)
 
     def getUserInput(self):
         userInput = input("Higher or Lower?: ")
@@ -35,5 +61,6 @@ class Director:
         elif (userInput == 'Lower' or userInput == 'lower'):
             return False
 
+# Would like this to have drawer draw a card and return current card
     def getCurrentCard(self):
         Drawer.getCurrentCard
