c53f9bf9f287bf639c412d809365a79f6f5901c9
matthewrapp <mattrapp25@gmail.com>
2021-02-04 15:50:05 -0700

    Director

---
+++ jumper_project/jumper/game/director.py
@@ -0,0 +1,26 @@
+# This is the director class
+# from game.drawer import Drawer
+
+"""
+Classes:
+ - Director
+ - Console (To print and receive inputs) // Austin
+ - WordHandler/Finx // Matthew 
+ - Parachute // Preston
+"""
+
+
+class Director:
+    """A code template for a person who directs the game. The responsibility of 
+    this class of objects is to keep track of the score and control the 
+    sequence of play.
+
+    Attributes:
+    """
+
+    def __init__(self):
+        """The class constructor.
+
+        Args:
+            self (Director): an instance of Director.
+        """
