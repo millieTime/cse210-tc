be830c878432d14157a6554034344417208461d3
Nitsua <pot19003@byui.edu>
2021-02-04 17:41:16 -0700

    word_display update

---
+++ jumper_project/jumper/game/wordhandler.py
@@ -0,0 +1,38 @@
+import random
+import re
+
+class WordHandler:
+
+    def __init__(self):
+        self.words = ['dictionary', 'basketball',
+                      'robinhood', 'hippo', 'rhinoceros', 'friend', 'movie', 'genius', 'heartattack', 'mountain', 'doorknob']
+        self.selectedWord = self.getWord()
+        self.guessedLetters = ['']
+        self.word = len(self.selectedWord)
+        self.hidden = ["_"] * self.word
+
+
+    def getWord(self):
+        self.selectedWord = random.choice(self.words)
+        return self.selectedWord
+
+    def checkLetter(self, userInput):
+        return userInput in self.selectedWord
+
+    def hasBeenGuessed(self, userInput):
+        print(userInput)
+        print(self.guessedLetters)
+        if userInput not in self.guessedLetters:
+            self.guessedLetters.append(userInput)
+            return True
+        else:
+            return False
+    
+    def word_display(self):
+        for spot in re.finditer(self.guessedLetters[-1],self.selectedWord):
+            index = spot.start()
+            self.hidden[index] = self.guessedLetters[-1]
+        printed_word = " ".join(self.hidden)
+        print(printed_word)
+
+        
