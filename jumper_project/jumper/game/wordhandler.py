48fd35ea6725b1da5ff89457c6dc6094ad125ef0
matthewrapp <mattrapp25@gmail.com>
2021-02-04 19:27:23 -0700

    adding comments to wordhandler

--- jumper_project/jumper/game/wordhandler.py
+++ jumper_project/jumper/game/wordhandler.py
@@ -3,8 +3,25 @@ import random
 
 
 class WordHandler:
+    """A code template to choose a word from a list of words everything we need from the word
+    ex) a character within a word, if that character has been guess, if that character is right or wrong
+    and if there is no more characters to be guessed, meaning the word is filled.
+
+    Attributes:
+        words: a list of random, hard-coded, words
+        selectedWord: calling the getWord() method
+        guessedLetters: an array of already guessed letters within the round
+        word: get the length of the word
+        hidden: hide the letters within the word until those letters are guessed
+    """
 
     def __init__(self):
+        """The class constructor.
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+        """
+
         self.words = ['dictionary', 'basketball',
                       'robinhood', 'hippo', 'rhinoceros', 'friend', 'movie', 'genius', 'heartattack', 'mountain', 'doorknob']
         self.selectedWord = self.getWord()
@@ -13,13 +30,33 @@ class WordHandler:
         self.hidden = ["_"] * self.word
 
     def getWord(self):
+        """Generates and picks a random word from the words list
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+        """
+
         self.selectedWord = random.choice(self.words)
         return self.selectedWord
 
     def checkLetter(self, userInput):
+        """Checks the letter guessed by the user to see if it's in the word.
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+            userInput: a parameter to take the userInput when the method is called.
+        """
+
         return userInput in self.selectedWord
 
     def canBeGuessed(self, userInput):
+        """Checks the letter and if it's already been guessed or not
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+            userInput: a parameter to take the userInput when the method is called.
+        """
+
         if userInput not in self.guessedLetters:
             self.guessedLetters.append(userInput)
             return True
@@ -27,6 +64,12 @@ class WordHandler:
             return False
 
     def word_display(self):
+        """Displays the word to the user. First off as a bunch of hidden letters and reveals the word as letters are guessed.
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+        """
+
         for spot in re.finditer(self.guessedLetters[-1], self.selectedWord):
             index = spot.start()
             self.hidden[index] = self.guessedLetters[-1]
@@ -34,6 +77,12 @@ class WordHandler:
         return printed_word
 
     def wordFound(self):
+        """Tells whether there are no more hidden letters, meaning that the word has been found!
+
+        Args:
+            self (WordHandler): an instance of WordHandler.
+        """
+
         if self.hidden.find("_"):
             return True
         else:
