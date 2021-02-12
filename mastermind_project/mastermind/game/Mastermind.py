82c2a178e112adb4b7da2459cf2275cc412d743b
matthewrapp <mattrapp25@gmail.com>
2021-02-12 12:21:02 -0700

    Mastermind class: done

--- mastermind_project/mastermind/game/Mastermind.py
+++ mastermind_project/mastermind/game/Mastermind.py
@@ -4,6 +4,13 @@ import random
 class Mastermind:
 
     def __init__(self):
+        """Class constructor. Declares and initializes instance attributes.
+
+        Args:
+            self (Mastermind): An instance of Mastermind.
+            self._items: An empty dictionary/object that will eventually store a key === playerName and a list of vlaues ===
+                         [generated secret code, player most recent guess, and hint once comparing the code and guess]
+        """
         self._items = {}
 
     def prepare(self, player):
@@ -19,20 +26,27 @@ class Mastermind:
         self._items[name] = [code, guess, hint]
 
     def to_string(self):
-        """ Most recent guess & most recent hint to print for console """
         """
-        Converts the board data to its string representation and returns it to the caller
+        Converts the game data to its string representation and returns it to the caller
 
         Args:
-        - self: an isntance of Board()
+        - self: an isntance of Mastermind()
         """
-        pass
+        text = '\n-------------------------'
+        text += '\n'
+        for key, value in self._items.items():
+            text += 'Player ' + key + ': ' + value[1]
+            text += ''
+            text += ', ' + value[2]
+            text += '\n'
+        text += '-------------------------\n'
+        return text
 
     def _create_hint(self, code, guess):
         """Generates a hint based on the given code and guess.
 
         Args:
-            self (Board): An instance of Board.
+            self (Mastermind): An instance of Mastermind.
             code (string): The code to compare with.
             guess (string): The guess that was made.
 
