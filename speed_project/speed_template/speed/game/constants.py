8d7ba9abbbf0cccfb46364cfb1e3453413ae3b0d
Nitsua <pot19003@byui.edu>
2021-02-18 16:27:59 -0700

    minor loss hotfix

--- speed_project/speed_template/speed/game/constants.py
+++ speed_project/speed_template/speed/game/constants.py
@@ -2,7 +2,7 @@ import os
 
 MAX_X = 60
 MAX_Y = 20
-FRAME_LENGTH = 1/1
+FRAME_LENGTH = 1/5
 STARTING_WORDS = 5
 PATH = os.path.dirname(os.path.abspath(__file__))
 LIBRARY = open(PATH + "/words.txt").read().splitlines()
