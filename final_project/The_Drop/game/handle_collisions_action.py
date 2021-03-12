fde0f8da8b438281052b5700b0b3414432566165
millieTime <thisismynewemailsoyeah@gmail.com>
2021-03-12 14:17:27 -0700

    added notes to draw_actors and handle_collisions

--- final_project/The_Drop/game/handle_collisions_action.py
+++ final_project/The_Drop/game/handle_collisions_action.py
@@ -16,43 +16,15 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        paddle = cast["paddle"][0]
-
-        balls_to_remove = []
-
-        for ball in cast["balls"]:
-            self._handle_wall_bounce(ball)
-            self._handle_paddle_bounce(ball, paddle)
-
-            bricks = cast["bricks"]
-            self._handle_brick_collision(ball, bricks)
-
-            if constants.BALLS_CAN_DIE and self._is_off_screen(ball):
-                balls_to_remove.append(ball)
-
-        for ball in balls_to_remove:
-            cast["balls"].remove(ball)
-
-    def _handle_wall_bounce(self, ball):
-        ball_x = ball.center_x
-        ball_y = ball.center_y
-
-        # Check for bounce on walls
-        if ball_x <= 0 or ball_x >= constants.MAX_X:
-            ball.bounce_vertical()
-
-        if ball_y >= constants.MAX_Y:
-            ball.bounce_horizontal()
-
-        if not constants.BALLS_CAN_DIE and ball_y <= 0:
-            ball.bounce_horizontal()
-
-    def _handle_paddle_bounce(self, ball, paddle):
-        # This makes use of the `Sprite` functionality
-        if paddle.collides_with_sprite(ball):
-            # Ball and paddle collide!
-            ball.bounce_horizontal()
-
+        #Yeah, this whole thing needs done.
+        # for drop_point in cast["drop_points"]:
+        #     if drop_point intersects with a beat:
+        #         remove the beat, add some points
+        #     if it didn't intersect with any:
+        #         subtract some points
+        return
+        
+    # Just here for reference.
     def _handle_brick_collision(self, ball, bricks):
         brick_to_remove = None
 
@@ -65,5 +37,8 @@ class HandleCollisionsAction(Action):
         if brick_to_remove != None:
             bricks.remove(brick_to_remove)
 
-    def _is_off_screen(self, ball):
-        return ball.center_y < 0
+    #could be super useful for if the beats were missed.
+    def _is_off_screen(self, beat):
+        # If the beat gets off the screen, it was missed.
+        # We'll need to subtract some points, but not here.
+        return beat.center_y < 0
