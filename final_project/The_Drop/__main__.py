0b69da45aa2df31a7373d9c471029c9b85dd8958
millieTime <thisismynewemailsoyeah@gmail.com>
2021-03-17 16:29:00 -0600

    using Matthew's song

--- final_project/The_Drop/__main__.py
+++ final_project/The_Drop/__main__.py
@@ -24,10 +24,10 @@ def main():
     # create the cast {key: tag, value: list}
     cast = {}
 
-    song = arcade.load_sound(constants.DIRROOT + "/assets/songs/Memory/memory.mp3")
+    song = arcade.load_sound(constants.DIRROOT + "/assets/songs/Coming_For_You/Coming_For_You.wav")
     
     beat_map = BeatMap()
-    beat_map.read_file(constants.DIRROOT + "/assets/songs/Memory/memory.txt")
+    beat_map.read_file(constants.DIRROOT + "/assets/songs/Coming_For_You/Coming_For_You.txt")
 
     cast["beats"] = beat_map.get_beats()
 
