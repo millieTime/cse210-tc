4edd01b11ebd2864f4e711ae5cac7a36928aa038
millieTime <thisismynewemailsoyeah@gmail.com>
2021-03-18 17:43:32 -0600

    switching songs for video

--- final_project/The_Drop/__main__.py
+++ final_project/The_Drop/__main__.py
@@ -24,15 +24,15 @@ def main():
     # create the cast {key: tag, value: list}
     cast = {}
     song = arcade.Sound(
-        constants.DIRROOT + "/assets/songs/Mayday/Mayday.mp3")
+        constants.DIRROOT + "/assets/songs/Coming_For_You/Coming_For_You.wav")
     
 
     beat_map = BeatMap()
-    beat_map.read_file(constants.DIRROOT + "/assets/songs/Mayday/Mayday_1.txt")
+    beat_map.read_file(constants.DIRROOT + "/assets/songs/Coming_For_You/Coming_For_You.txt")
 
     cast["beats"] = beat_map.get_beats()
     cast["beats"].reverse()
-    
+
     keys = ['q', 'w', 'e', 'r']
     cast['drop_points'] = []
     for key in keys:
