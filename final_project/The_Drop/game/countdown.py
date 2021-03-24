7e03b90de0ecbad70346694eb3e7e33e27de4f95
millieTime <thisismynewemailsoyeah@gmail.com>
2021-03-24 11:23:19 -0600

    brief commit, new song

--- final_project/The_Drop/game/countdown.py
+++ final_project/The_Drop/game/countdown.py
@@ -4,3 +4,5 @@ from game import constants
 class Countdown(arcade.Sprite):
     # want this to show 3, 2, 1, so I'll need to override update and increment some timer to keep track of the number here.
     # Won't have pictures so much as it will have text that starts big nd gets small.
+    def __init__(self):
+        
